120ba2ea0e7c5b6522ae617c16f51b85fbfcccb9
yhx-coder <772144376@qq.com>
2021-08-30 21:24:36 +0800

    加入了功能菜单选项，以备扩展

--- src/main/java/com/example/handler/ClientChatHandler.java
+++ src/main/java/com/example/handler/ClientChatHandler.java
@@ -6,12 +6,16 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.util.AttributeKey;
 
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
 
 /**
  * @author: ming
  * @date: 2021/8/27 18:16
  */
 public class ClientChatHandler extends SimpleChannelInboundHandler<Message> {
+
+    private CountDownLatch latch = new CountDownLatch(1);
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, Message msg) throws Exception {
@@ -28,50 +32,60 @@ public class ClientChatHandler extends SimpleChannelInboundHandler<Message> {
             System.out.println(loginRes.getResponse());
             if (loginRes.getStatus() == LoginRes.LoginStatus.SUCCESS) {
                 ctx.channel().attr(AttributeKey.<Integer>valueOf("userId")).set(loginRes.getUserId());
-                sendMsg(ctx);
+                latch.countDown();
             } else {
-                login(ctx);
+                System.out.println("登录失败");
+                ctx.close();
             }
         } else if (messageType == Message.MessageType.MSG_RX) {
             MsgRX msgRX = msg.getMsgRX();
             System.out.println("用户" + msgRX.getSUserId() + "说: " + msgRX.getContent());
-            sendMsg(ctx);
         }
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        login(ctx);
-    }
-
-    private void sendMsg(ChannelHandlerContext ctx) {
-        Scanner scanner = new Scanner(System.in);
-        new Thread(()->{
-            while (true) {
-                System.out.print("请输入接收方的userId: ");
-                int dUserId = scanner.nextInt();
-                System.out.print("请输入要发送的消息, 按回车结束: ");
-                scanner.nextLine();         // 就很烦，要接收 nextInt() 中的 \n
-                String line = scanner.nextLine();
-
-                Integer sUserId = ctx.channel().attr(AttributeKey.<Integer>valueOf("userId")).get();
-
-                MsgReq msgReq = Message.newBuilder().getMsgReq()
-                        .newBuilderForType()
-                        .setSUserId(sUserId)
-                        .setMsg(line)
-                        .setDUserId(dUserId)
-                        .build();
-
-
-                Message message = Message.newBuilder()
-                        .setMessageType(Message.MessageType.MSG_REQ)
-                        .setMsgReq(msgReq)
-                        .build();
-                ctx.channel().writeAndFlush(message);
+        new Thread(() -> {
+            login(ctx);
+            System.out.println(latch);
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-        }).start();
+
+            while (true) {
+                menu();
+
+                Scanner scanner = new Scanner(System.in);
+                String line = scanner.nextLine();
+                String[] s = line.split(" ");
+                switch (s[0]) {
+                    case "send": {
+                        MsgReq req = Message.newBuilder()
+                                .getMsgReq().newBuilderForType()
+                                .setDUserId(Integer.parseInt(s[1]))
+                                .setMsg(s[2])
+                                .build();
+
+                        Message msgReq = Message.newBuilder()
+                                .setMessageType(Message.MessageType.MSG_REQ)
+                                .setMsgReq(req)
+                                .build();
+
+                        ctx.channel().writeAndFlush(msgReq);
+                        break;
+                    }
+                    case "quit":{
+                        System.out.println("您已退出聊天！");
+                        ctx.close();
+                        return;
+                    }
+                }
+            }
+        }, "waitForSelection").start();
     }
+
 
     private void login(ChannelHandlerContext ctx) {
         Scanner scanner = new Scanner(System.in);
@@ -81,15 +95,23 @@ public class ClientChatHandler extends SimpleChannelInboundHandler<Message> {
         System.out.print("请输入UserId: ");
         int userId = scanner.nextInt();
 
-        LoginReq loginReq = Message.newBuilder().getLoginReq().newBuilderForType()
-                .setUserId(userId)
+        // 一定要分两段构建消息，否则要报错。因为要解析的是 Message 。 若一次构建则是 LoginReq 了。
+        LoginReq req = Message.newBuilder()
+                .getLoginReq().newBuilderForType()
                 .setUsername(username)
+                .setUserId(userId)
                 .build();
 
-        Message message = Message.newBuilder()
+        Message loginReq = Message.newBuilder()
                 .setMessageType(Message.MessageType.LOGIN_REQ)
-                .setLoginReq(loginReq)
+                .setLoginReq(req)
                 .build();
-        ctx.channel().writeAndFlush(message);
+
+        ctx.channel().writeAndFlush(loginReq);
+    }
+
+    private void menu() {
+        System.out.println("send [userId] [message]");
+        System.out.println("quit");
     }
 }
